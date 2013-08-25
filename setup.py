4e507b0247b68d49c18c5df6915efd849151dbb8
Ovidiu Predescu <ovidiu@gmail.com>
2013-08-24 18:04:10 -0700

    Use setuptools instead of distutils so we can build an egg file (python setup.py bdist_egg).

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import numpy
-from distutils.core import setup, Extension
-from Cython.Distutils import build_ext
+
+from setuptools import setup
+from distutils.extension import Extension
+
+# setuptools DWIM monkey-patch madness
+# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
+import sys
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
 
 define_macros = [('VOID', 'int'),
                  ('REAL', 'double'),
@@ -8,27 +16,25 @@ define_macros = [('VOID', 'int'),
                  ('TRILIBRARY', 1),
                  ('ANSI_DECLARATORS', 1)]
 
+cytriangle = Extension(
+    'triangle.core',
+    sources = ['c/triangle.c', 'triangle/c_triangle.pxd', 'triangle/core.pyx'],
+    include_dirs = ['c'],
+    define_macros = define_macros)
+
 setup(name='triangle',
-    packages=['triangle'],
-    package_dir={'triangle':'triangle'},
-    package_data={'triangle':['data/*.node',
-                             'data/*.ele', 
-                             'data/*.poly',
-                             'data/*.area',
-                             'data/*.edge',
-                             'data/*.neigh']},
-    version='2013.01.07',
-    description='Python binding to the triangle library',
-    author='Dzhelil Rufat',
-    author_email='drufat@caltech.edu',
-    license='GNU LGPL',
-    url='http://drufat.github.com/triangle',
-    requires = ['numpy (>=1.7.0)',
-                'cython (>=0.18)'],
-    cmdclass = {'build_ext': build_ext},
-    ext_modules=[
-                 Extension('triangle.core', ['c/triangle.c', 'triangle/c_triangle.pxd', 'triangle/core.pyx'],
-                           include_dirs = ['c'],
-                           define_macros = define_macros)
-    ]
+      packages=['triangle'],
+      package_dir={'triangle':'triangle'},
+      version='2013.01.07',
+      description='Python binding to the triangle library',
+      author='Dzhelil Rufat',
+      author_email='drufat@caltech.edu',
+      license='GNU LGPL',
+      url='http://drufat.github.com/triangle',
+      requires = ['numpy (>=1.7.0)',
+                  'cython (>=0.18)'],
+      setup_requires=[
+          'setuptools_cython',
+      ],
+      ext_modules=[ cytriangle ]
 )
